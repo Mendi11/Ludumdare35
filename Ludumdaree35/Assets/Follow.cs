1c0c9947f78fd22244890588a779df6402a968cc
unknown <gashar@3se-spel-05.utb.fuv.se>
2016-04-16 14:58:13 +0200

    Fixed Camera

---
+++ Ludumdaree35/Assets/Follow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    Transform m_Player;
+    Camera m_Camera;
+
+    float dampTime = 0.3f; //offset from the viewport center to fix damping
+    Vector3 velocity = Vector3.zero;
+
+
+    // Use this for initialization
+    void Start () {
+        m_Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().transform;
+        m_Camera = GetComponent<Camera>();
+	}
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        if (m_Player)
+        {
+            Vector3 point = m_Camera.WorldToViewportPoint(m_Player.position);
+            Vector3 delta = m_Player.position - m_Camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+
+    }
+
+	void Update () {
+
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+}
--- Ludumdaree35/Assets/Scripts/Movement.cs
+++ Ludumdaree35/Assets/Scripts/Movement.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+    [SerializeField]
+    private float m_Speed;
+
+    Vector2 m_Movement;
+    Rigidbody2D m_Rgb;
+
+
 	// Use this for initialization
 	void Start () {
-	
+        m_Speed = 5f;
+        m_Rgb = GetComponent<Rigidbody2D>();
+
 	}
 	
+
+    void FixedUpdate()
+    {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+       m_Movement = new Vector2(h, v).normalized;
+        m_Rgb.velocity = m_Movement * m_Speed;
+
+    }
 	// Update is called once per frame
 	void Update () {
-	
-	}
+      
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+   
+    }
+    void OnCollisionExit(Collision col)
+    {
+    }
 }
